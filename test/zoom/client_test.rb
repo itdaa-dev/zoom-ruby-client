50b7b3a887b9a53cc635be60531c4fec4677daa1
Jay9393 <tlswo1313@gmail.com>
2022-02-28 17:06:30 +0900

    Fix: version 업데이트

--- test/zoom/client_test.rb
+++ test/zoom/client_test.rb
@@ -4,6 +4,6 @@ require "test_helper"
 
 class Zoom::ClientTest < Minitest::Test
   def test_that_it_has_a_version_number
-    assert_equal "0.4.1", ::Zoom::VERSION
+    assert_equal "0.4.2", ::Zoom::VERSION
   end
 end
